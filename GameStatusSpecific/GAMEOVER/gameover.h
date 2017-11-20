e90a1b769d4f3e5b85ed72ea01e48ce50983f43a
Fauszt Benedek <fausztb@gmail.com>
2017-11-20 19:38:39 +0100

    gameover.h elsőre

---
+++ GameStatusSpecific/GAMEOVER/gameover.h
@@ -0,0 +1,20 @@
+#ifndef GAMEOVER_H
+#define GAMEOVER_H
+
+#include <SDL.h>
+#include <SDL_gfxPrimitives.h>
+#include <SDL_ttf.h>
+
+#include "../Button.h"
+
+typedef struct gameOverScreen{
+    button toMenu, newGame;
+}
+
+gameOverScreen gameOverScreen_init();
+
+void gameOverScreen_drw(SDL_Surface *screen, gameOverScreen this);
+
+void gameOverScreen_upd(*gameOverScreen this, SDL_Event ev);
+
+#endif GAMEOVER_H

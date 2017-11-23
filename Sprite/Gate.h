09934eff676d6626228a153b790e98e955af8f85
Fauszt Benedek <fausztb@gmail.com>
2017-11-23 18:48:13 +0100

    Gate komment

--- Sprite/Gate.h
+++ Sprite/Gate.h
@@ -5,14 +5,19 @@
 #include <SDL_gfxPrimitives.h>
 
 /**A jobb oldalon látható kapu vagy célvonal adatait tároló struktúra
+*A sikeres átjutást tesztelő függvény nem ebben a modulban, hanem a gameStatusChangingFunctions modulban található.
 */
 typedef struct Gate{
     float upper;///<Felső határ y koordinátája
     float lower;///<Alsó határ y koordinátája
 }Gate;
 
+/**Inicializálja a Gate struktúrában lévő értékeket
+*/
 Gate gate_init(float upper, float lower);
 
+/**Kirajzolja a képernyőre a célvonalat
+*/
 void gate_drw(SDL_Surface *screen, Gate * gate);
 
 #endif // GATE_H

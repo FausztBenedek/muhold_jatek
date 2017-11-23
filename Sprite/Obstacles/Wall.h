d8e58ea63242a8357afbce84f6952f554e133a58
Fauszt Benedek <fausztb@gmail.com>
2017-11-23 22:24:51 +0100

    Falak dokumentálva

--- Sprite/Obstacles/Wall.h
+++ Sprite/Obstacles/Wall.h
@@ -10,7 +10,8 @@
 ///@defgroup wall Falak
 ///@{
 
-
+/**A fal struktúra az egyetlen akadály ebben a verzióban.
+*/
 typedef struct Wall{
     float x;
     float y;
@@ -18,10 +19,17 @@ typedef struct Wall{
     float h;///<Magasság
 }Wall;
 
+/**Inicializálja a falat (nem a fájlból, hanem a paraméterlistáról)
+*/
 Wall wall_init(float x, float y, float w, float h);
 
+/**Kirajzolja a falat a képernyőre
+*/
 void wall_drw(SDL_Surface *screen, Wall * wall);
 
+/**Megállapítja, hogy az egér a fal felett van-e.
+*@return true, ha benne van az egér a falban és false, ha nem.
+*/
 bool wall_hover(Wall * const wall, SDL_Event ev);
 
 /**Megvizsgálja, hogy melyik egy adott falnak a legközelebbi pontja egy körhöz.

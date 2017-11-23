685875f05429dbff0f9d5b8a1211216eac4633db
Fauszt Benedek <fausztb@gmail.com>
2017-11-23 09:54:06 +0100

    GameStatus enum komment2

--- GameStatusSpecific/GameStatus.h
+++ GameStatusSpecific/GameStatus.h
@@ -1,14 +1,17 @@
 #ifndef GAME_STATUS_H
 #define GAME_STATUS_H
 
-///
+/**A játék lehetséges állapotait tartalmazó enum.
+*A fő programban egyetlen egy gameStatus változó van, aminek az értékét ellenőrzik a while loop-ok
+*és ez dönti el, hogy melyik állapotban van a játék.
+*/
 enum gameStatus {
-    QUITTING,
-    RUNNING,
-    SETTING,
-    MENU,
-    WINNING,
-    GAMEOVER
+    QUITTING,///<Kilépés
+    RUNNING,///<Megy a műhold
+    SETTING,///<Bolygók beállítása
+    MENU,///<Menü
+    WINNING,///<"Sikerült" képernyő
+    GAMEOVER///<"Vesztettél" képernyő
 };
 
 #endif // GAME_STATUS_H

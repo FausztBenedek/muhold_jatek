166aec93482c479c78c0c04d04acec610d6f817d
Fauszt Benedek <fausztb@gmail.com>
2017-11-23 10:08:31 +0100

    Gombok a winning screennél felszabadítva

--- GameStatusSpecific/WINNING/winning.c
+++ GameStatusSpecific/WINNING/winning.c
@@ -18,7 +18,11 @@ void winningScreen_drw(SDL_Surface *screen, WinningScreen this, TTF_Font *font){
 }
 
 void winningScreen_upd(WinningScreen *this, SDL_Event ev){
-        button_upd(&this->nextLevel, ev);
-        button_upd(&this->toMenu, ev);
+    button_upd(&this->nextLevel, ev);
+    button_upd(&this->toMenu, ev);
+}
 
+void winningScreen_cleanup(WinningScreen *this){
+    button_cleanup(&this->toMenu);
+    button_cleanup(&this->nextLevel);
 }
--- GameStatusSpecific/WINNING/winning.h
+++ GameStatusSpecific/WINNING/winning.h
@@ -27,4 +27,8 @@ void winningScreen_drw(SDL_Surface *screen, WinningScreen this, TTF_Font *font);
 */
 void winningScreen_upd(WinningScreen *this, SDL_Event ev);
 
+/**Feltakarítja a gombjai által lefoglalt dinamikus memóriát.
+*/
+void winningScreen_cleanup(WinningScreen *this);
+
 #endif // WINNING_H

a9747d90ad36ce175f55b1790594e730135bd1b6
Fauszt Benedek <fausztb@gmail.com>
2017-11-28 10:27:58 +0100

    Kicsomagolás a leadottból

--- Storage/Data.c
+++ Storage/Data.c
@@ -47,10 +47,10 @@ static void read_from_file(int *numOf_level, bool *solvedarr, int *attemptsarr){
     }
 
     for (i = 0; i < *numOf_level; i++){
-        if (fscanf(fp, "%d. Szint:\n", &j)) perror("fscanf problem");
+        fscanf(fp, "%d. Szint:\n", &j);
         if (0 == fscanf(fp, "\tSikertelen probalkozasok szama:\t%d\n", &attemptsarr[i]))    attemptsarr[i] = 0;
         if (0 == fscanf(fp, "\tTeljesitett szint:\t%s", checker))                           strcpy(checker, "Nem");
-        if (0 == fscanf(fp, "\n")) perror("fscanf problem");
+        fscanf(fp, "\n");
 
         if (NULL != strstr(checker, "Igen"))    solvedarr[i] = true;
         else                        solvedarr[i] = false;
